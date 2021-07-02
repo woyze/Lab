6ce9267d561c327f82a203be7a359644dcb0d1ab
woyze <86857951+woyze@users.noreply.github.com>
2021-07-03 01:42:59 +0300

    Add files via upload

---
+++ lab3_2.cpp
@@ -0,0 +1,27 @@
+﻿
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    string s;
+
+    getline(cin, s);
+
+   
+    transform(s.begin(), s.end(), s.begin(), ::tolower);
+    string result = "";
+
+    
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'y') {
+            result += s[i];
+        }
+    }
+   
+    cout << result;
+}
+
